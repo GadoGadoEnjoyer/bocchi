4ac75e65544b6f5ffdb31afad55099bde635c51e
Reza <alirezaarifi69@gmail.com>
2023-10-24 09:12:37 +0700

    Style :D

--- app/views/community/index.php
+++ app/views/community/index.php
@@ -5,21 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Community</title>
     <style>
-        body{
-            background-color: #f5f5f5;
-        }
         .lol{
             display: flex;
             margin-top:10%;
-            align-items: center;
-            background-color: black;
         }
         img{
             width: 25%;
             border-radius: 30px;
         }
         h1{
-            color: white;
             text-align: center;
         }
     </style>
