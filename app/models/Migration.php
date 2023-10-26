756b2fda9fa1baebfa710db50620e8a99819283e
Reza <alirezaarifi69@gmail.com>
2023-10-26 10:47:39 +0700

    Yes

--- app/models/Migration.php
+++ app/models/Migration.php
@@ -27,22 +27,6 @@ class Migration{
                     token VARCHAR(255) NOT NULL UNIQUE
                 )');
             }
-            $this->db->execute();   
-            if(!$this->tableExist('Character')){
-                $this->db->query('CREATE TABLE `Character`(
-                    character_id INT AUTO_INCREMENT PRIMARY KEY,
-                    fullname VARCHAR(100) NOT NULL,
-                    `name` VARCHAR(100) NOT NULL,
-                    photo VARCHAR(255) NOT NULL DEFAULT "default.jpg",
-                    first_appearance VARCHAR(100) NOT NULL,
-                    birthday VARCHAR(100) NOT NULL,
-                    gender VARCHAR(50) NOT NULL,
-                    height INT NOT NULL,
-                    `weight` INT NOT NULL,
-                    hair_color VARCHAR(100) NOT NULL,
-                    eye_color VARCHAR(100) NOT NULL
-                )');
-            }
             $this->db->execute();
             if(!$this->tableExist('Post')){
                 $this->db->query('CREATE TABLE `Post`(
@@ -56,18 +40,12 @@ class Migration{
                 )');
             }
             $this->db->execute();
-            if($this->tableExist('Character') && $this->tableExist('Post') && $this->tableExist('User')){
+            if($this->tableExist('Post') && $this->tableExist('User')){
                 $this->db->query('ALTER TABLE `Post` ADD FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;');
                 $this->db->execute();
                 $this->db->query('INSERT INTO `User` (`user_id`, `username`, `password`, `email`, `verified`, `role`, `photo`, `token`) VALUES (1, "admin", "$2a$12$iQ8W.0LlrpA1G7cHqM6fuOlYkoF2JsLBQ/fd4kkg56M5yVNF3us5O", "alirezaarifi69@gmail.com", 1, "admin", "default.jpg", "--");');
                 $this->db->execute();
                 $this->db->query('INSERT INTO `User` (`user_id`, `username`, `password`, `email`, `verified`, `role`, `photo`, `token`) VALUES (2, "anon", "$2a$12$iQ8W.0LlrpA1G7cHqM6fuOlYkoF2JsLBQ/fd4kkg56M5yVNF3us5O", "koplakreza406@gmail.com", 1, "user", "default.jpg", "-");'); 
-                $this->db->execute();
-                $this->db->query('INSERT INTO `Character` (fullname, `name`, photo, first_appearance, birthday, gender, height, `weight`, hair_color, eye_color) 
-                VALUES ("LoafAnon", "Loaf", "default.jpg", "Episode 69", "2001-09-11", "Male", "727", "69", "black", "black")');
-                $this->db->execute();
-                $this->db->query('INSERT INTO `Character` (fullname, `name`, photo, first_appearance, birthday, gender, height, `weight`, hair_color, eye_color) 
-                VALUES ("SisterAnon", "Dodol", "default.jpg", "Episode 69", "2001-01-01", "Male", "800", "80", "black", "nonexistent")');
                 $this->db->execute();
             }
 
