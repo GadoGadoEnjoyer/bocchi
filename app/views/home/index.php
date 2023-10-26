b46e0de540ddeb400bf22b35ed1f6b64a58f4a24
Reza <alirezaarifi69@gmail.com>
2023-10-26 08:34:02 +0700

    First Test

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,22 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bocchi</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/index.css">
 </head>
 <body>
-    <p>Some Bocchi Image :D </p>
-    <?php 
-        foreach($data as $img){
-            echo '<img src=https://safebooru.org//images/'. $img['directory'] .'/'. $img['image'] . '?'. $data['id'] . ' width=150>';
-        }
-    ?>
-    <p>See some Uploaded pic from community!</p>
-    <?php echo '<a href="' . BASEURL . '/community">Community</a>'; ?>
-    <p>Upload your own pic or video! (Butuh akun tapi)</p>
-    <?php echo '<a href="' . BASEURL . '/community/post">Upload</a>'; ?>
-    <p>Register Akun</p>
-    <?php echo '<a href="' . BASEURL . '/register">Register</a>'; ?>
-    <p>Login Akun</p>
-    <?php echo '<a href="' . BASEURL . '/login">Login</a>'; ?>
+    <header class="header">
+        <nav class="navigation">
+            <a href="#">About</a>
+            <a href="#">Music</a>
+            <img src="../assets/image/logo-image/Bocchi_the_Rock!_logo.svg.png" alt="" class="headerimg">
+            <a href="../html/character.html">Gallery</a>
+            <a href="#">Community</a>
+        </nav>
+        <div class="user-icon">
+        
+        </div>
+    </header>
+    <section class="home">  
+        <div class="homecontent">
+            <div class="image">
+                <img src="../assets/image/logo-image/logo.png" alt="">
+            </div>
+            
+        
+        <div class="descriptions">
+            <p class="description">
+                Bocchi the Rock! is a charming manga and anime series that follows the journey of a socially anxious high school girl named Hitori "Bocchi" Gotō as she decides to learn guitar and sing in a band. Though she lacks confidence and experience, Bocchi's kindhearted spirit and passion for music motivates her to step outside her comfort zone.
+            </p>
+            <p class="description">
+                The story illustrates Bocchi's endearing personality as someone who imagines absurd scenarios due to her anxiety, but genuinely wants to make friends and bring joy to people. She recruits some classmates - the easygoing drummer Nijika, skilled bassist Ryou, and lead guitarist Ikuyo who secretly longs to be a singer - to join her makeshift band for their school festival.
+            </p>
+        </div>
+        </div>
+        
+        
+    </section>
 </body>
 </html>
