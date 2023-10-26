a47a9cff069df532ad3488aff26392113b351f38
Reza <alirezaarifi69@gmail.com>
2023-10-26 09:04:43 +0700

    OOga booga

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="<?php echo(BASEURL . "/css/index.css")?><? echo(BASEURL ."")?><? echo(BASEURL .
 </head>
 <body>
     <header class="header">
